66a5018be6bbb8211567e0a50783284a6b2e0259
Pedro Sobral <pedrolusobral85@gmail.com>
2026-08-04 16:39:35 -0300

    feat: add user repository

---
+++ backend/src/modules/user/user.repository.js
@@ -0,0 +1,45 @@
+import { prisma } from "../../shared/lib/prisma.js";
+
+async function getUserById(id){
+    const user = await prisma.user.findUnique({
+        where: { id }
+    });
+    return user;
+}
+
+async function getUserByEmail(email){
+    const user = await prisma.user.findUnique({
+        where: { email }
+    });
+    return user;
+}
+
+async function createUser(userData){
+    const user = await prisma.user.create({
+        data: userData
+    });
+    return user;
+}
+
+async function updateUser(id, userData){
+    const user = await prisma.user.update({
+        where: { id },
+        data: userData
+    });
+    return user;
+}
+
+async function deleteUser(id) {
+    await prisma.user.update({
+        where: { id },
+        data: { deletedAt: new Date() }
+    });
+}
+
+export default {
+    getUserById,
+    getUserByEmail,
+    createUser,
+    updateUser,
+    deleteUser
+}
