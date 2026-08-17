83c5d8d9e7865c60ae39e74e7bbb10ee1114bc81
Pedro Sobral <pedrolusobral85@gmail.com>
2026-08-17 16:15:06 -0300

    feat: add dependency injection in user repository

--- backend/src/modules/user/user.repository.js
+++ backend/src/modules/user/user.repository.js
@@ -1,45 +1,45 @@
-import { prisma } from "../../shared/lib/prisma.js";
+export default function createUserRepository(prisma){
+    async function getUserById(id){
+        const user = await prisma.user.findUnique({
+            where: { id }
+        });
+        return user;
+    }
 
-async function getUserById(id){
-    const user = await prisma.user.findUnique({
-        where: { id }
-    });
-    return user;
-}
+    async function getUserByEmail(email){
+        const user = await prisma.user.findUnique({
+            where: { email }
+        });
+        return user;
+    }
 
-async function getUserByEmail(email){
-    const user = await prisma.user.findUnique({
-        where: { email }
-    });
-    return user;
-}
+    async function createUser(userData){
+        const user = await prisma.user.create({
+            data: userData
+        });
+        return user;
+    }
 
-async function createUser(userData){
-    const user = await prisma.user.create({
-        data: userData
-    });
-    return user;
-}
+    async function updateUser(id, userData){
+        const user = await prisma.user.update({
+            where: { id },
+            data: userData
+        });
+        return user;
+    }
 
-async function updateUser(id, userData){
-    const user = await prisma.user.update({
-        where: { id },
-        data: userData
-    });
-    return user;
-}
+    async function deleteUser(id) {
+        await prisma.user.update({
+            where: { id },
+            data: { deleteAt: new Date() }
+        });
+    }
 
-async function deleteUser(id) {
-    await prisma.user.update({
-        where: { id },
-        data: { deleteAt: new Date() }
-    });
-}
-
-export default {
-    getUserById,
-    getUserByEmail,
-    createUser,
-    updateUser,
-    deleteUser
+    return {
+        getUserById,
+        getUserByEmail,
+        createUser,
+        updateUser,
+        deleteUser
+    }
 }
